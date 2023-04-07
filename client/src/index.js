e68b9aa0c305c3872b816743554d3cea62f33a8b
ahmadad62 <a.naghian@gmail.com>
2023-04-07 12:53:10 +0200

    was done

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import { Provider } from 'react-redux'
-// import store from "./redux/store"
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// );
 
 import React from "react";
 import ReactDOM from "react-dom";
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjY3MzA2ZjA1OTkyM2MzNGMzMzk0MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDU0MzIzMiwiZXhwIjoxNjgwODAyNDMyfQ.h-oz-_uTHzupIiVcRZALvae39EMWtnFtCU0-TkWRfXc'
+//const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjY3MzA2ZjA1OTkyM2MzNGMzMzk0MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDU0MzIzMiwiZXhwIjoxNjgwODAyNDMyfQ.h-oz-_uTHzupIiVcRZALvae39EMWtnFtCU0-TkWRfXc'
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
