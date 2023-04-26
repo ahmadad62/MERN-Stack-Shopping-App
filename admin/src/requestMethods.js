b0092c8c6dac8c15d6fdbe9abd30f981f76a864a
ahmadad62 <a.naghian@gmail.com>
2023-04-26 23:01:43 +0200

    change token

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjY3MzA2ZjA1OTkyM2MzNGMzMzk0MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MTAyMjExMSwiZXhwIjoxNjgxMjgxMzExfQ.Fk6MK_jZM_f0OGNoMiTaAuxlhrMruI5dgXeAiFJc1u8'
+// const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'https://mern-stack-shopping-app-api.onrender.com/api/';
+
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjY3MzA2ZjA1OTkyM2MzNGMzMzk0MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MjUyMDU0OCwiZXhwIjoxNjgyNzc5NzQ4fQ.8w6SSkFV6lZsK9eYGi60GawS37NQXJ0u9Y3-OP-AMe0'
 //const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 
